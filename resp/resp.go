10d28c2b4430bc457992a85468143d10851359f4
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-08-18 01:18:51 +0300

    feat: read an array, added github actions

--- resp/resp.go
+++ resp/resp.go
@@ -10,13 +10,14 @@ import (
 // Type struct which represents type of resp being parsed
 type Type byte
 
-// const (
-// 	SimpleString Type = '+'
-// 	Error        Type = '-'
-// 	Integer      Type = ':'
-// 	BulkString   Type = '$'
-// 	Array        Type = '*'
-// )
+// For user to compare Value.Type()
+const (
+	SimpleString Type = '+'
+	Error        Type = '-'
+	Integer      Type = ':'
+	BulkString   Type = '$'
+	Array        Type = '*'
+)
 
 // Value struct stores all info of resp parsed
 type Value struct {
@@ -26,6 +27,10 @@ type Value struct {
 	Array   []Value
 	Err     error
 	IsNull  bool
+}
+
+func (value *Value) Type() Type {
+	return value.typ
 }
 
 // RespReader struct is the buffer which is used to parse
@@ -44,30 +49,42 @@ func (resp *RespReader) ReadValue() (Value, error) {
 	if err != nil {
 		return Value{IsNull: true}, err
 	}
-	fmt.Println(char == ':')
-	if char == '*' {
-		return resp.ReadValue()
-	} else {
-		switch char {
-		case ':':
-			return resp.readInteger()
-		case '+':
-			return resp.readString()
-		case '$':
-			return resp.readBulkString()
-		case '-':
-			return resp.readErrorMessage()
-		}
+	switch char {
+	case ':':
+		return resp.readInteger()
+	case '+':
+		return resp.readString()
+	case '$':
+		return resp.readBulkString()
+	case '-':
+		return resp.readErrorMessage()
+	case '*':
+		return resp.readArray()
 	}
 	return Value{IsNull: true}, fmt.Errorf("parsing error: beginning doesn't follow resp convensions")
 }
 
-// func (resp *RespReader) readArray() (Value, error) {
+func (resp *RespReader) readArray() (Value, error) {
+	l, err := resp.readInt()
+	if err != nil {
+		return Value{IsNull: true}, err
+	}
+	if l < 0 {
+		return Value{typ: '$', IsNull: true}, fmt.Errorf("parsing error: array cannot have negative length")
+	}
 
-// }
+	var array []Value
+	for i := 0; i < l; i++ {
+		value, err := resp.ReadValue()
+		if err != nil {
+			return Value{IsNull: true}, fmt.Errorf("error parsing array:" + err.Error())
+		}
+		array = append(array, value)
+	}
+	return Value{typ: '*', Array: array}, err
+}
 
 func (resp *RespReader) readInteger() (Value, error) {
-	fmt.Println("hello")
 	num, err := resp.readInt()
 	if err != nil {
 		return Value{IsNull: true}, err
